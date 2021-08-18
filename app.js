f7d6237f7eea8ba2b12a32df3260af058b993e09
dim50dim <zheliezkov@gmail.com>
2021-08-18 12:04:21 +0300

    first try

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+
+
+const filePath = path.join(__dirname, 'boys', 'anna.txt')
+const  filePathNew= path.join(__dirname, 'girls','anna.txt')
+const folderNewPath = path.join(__dirname,'girls')
+
+const readPromise = util.promisify(fs.readFile);
+///////////  JSON file
+const man = {
+    name: 'kate',
+    age: 22,
+    department: ' history',
+    gender : 'female',
+}
+// fs.writeFileSync('boys/kate.txt',JSON.stringify(man), err=>{
+//     if(err) console.log('Error')
+// } );
+readPromise(filePath).then(data=>{
+    console.log(data)
+})
